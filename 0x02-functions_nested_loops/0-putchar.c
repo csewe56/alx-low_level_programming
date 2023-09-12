c336f18172fa0f98f053508c30db82bab43a2df9
csewe56 <csewe56@gmail.com>
2023-09-12 13:20:19 -0700

    a program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Prints the string _putchar
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * main _putchar file
+ * Return: 0
+ */
+int _putchar(char c);
+#endif
