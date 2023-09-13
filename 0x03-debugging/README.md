e3b9d28d41dffbf2efad0b67ba3909d257004d3b
csewe56 <csewe56@gmail.com>
2023-09-13 09:53:23 -0700

    0-main.c

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,2 @@
+0x03. C - Debugging
+finding and fixing errors in software that prevents it from running correctly
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,9 @@
+#ifndef_MAIN_H
+#define_MAIN_H
+
+/**
+ * main libriary
+ */
+
+void print_alphabet(void);
+#endif
