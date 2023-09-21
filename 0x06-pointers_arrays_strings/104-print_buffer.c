c5abc6b5324d31aad676fabceb65814328e16414
csewe56 <csewe56@gmail.com>
2023-09-21 12:13:00 -0700

    a function that prints a buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -11,64 +11,77 @@ void print_chars(char *b, int size, int i);
  */
 void print_buffer(char *b, int size)
 {
-    int i;
+	int i;
 
-    if (size <= 0)
-    {
-        putchar('\n');
-        return;
-    }
+	if (size <= 0)
+	{
+		putchar('\n');
+		return;
+	}
 
-    for (i = 0; i < size; i += 10)
-    {
-        printf("%08x: ", i);
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
 
-        print_hex(b, size, i);
+		print_hex(b, size, i);
 
-        print_chars(b, size, i);
-    }
+		print_chars(b, size, i);
+	}
 }
 
+/**
+ * print_hex - Prints the hexadecimal representation of buffer contents.
+ * @b: The buffer to print.
+ * @size: The size of the buffer.
+ * @i: The current position in the buffer.
+ */
 void print_hex(char *b, int size, int i)
 {
-    int j;
+	int j;
 
-    for (j = 0; j < 10; j++)
-    {
-        if (i + j < size)
-        {
-            printf("%02x", (unsigned char)b[i + j]);
-        }
-        else
-        {
-            printf("  ");
-        }
+	for (j = 0; j < 10; j++)
+	{
+		if (i + j < size)
+		{
+			printf("%02x", (unsigned char)b[i + j]);
+		}
+		else
+		{
+			printf("  ");
+		}
 
-        if (j % 2 == 1)
-        {
-            printf(" ");
-        }
-    }
+		if (j % 2 == 1)
+		{
+			printf(" ");
+		}
+	}
 }
 
+/**
+ * print_chars - Prints the ASCII characters from the buffer.
+ * @b: The buffer to print.
+ * @size: The size of the buffer.
+ * @i: The current position in the buffer.
+ */
 void print_chars(char *b, int size, int i)
 {
-    int j;
+	int j;
 
-    for (j = 0; j < 10; j++)
-    {
-        if (i + j < size)
-        {
-            if (b[i + j] >= 32 && b[i + j] <= 126)
-            {
-                putchar(b[i + j]);
-            }
-            else
-            {
-                putchar('.');
-            }
-        }
-    }
-    putchar('\n');
+	for (j = 0; j < 10; j++)
+	{
+		if (i + j < size)
+		{
+			if (b[i + j] >= 32 && b[i + j] <= 126)
+			{
+				putchar(b[i + j]);
+			}
+			else
+			{
+				putchar('.');
+			}
+		}
+	}
+	putchar('\n');
 }
+
 
