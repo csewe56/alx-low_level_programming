34d923ff0d540d857409996ccf30b7fc11976b4b
csewe56 <csewe56@gmail.com>
2023-09-21 05:13:14 -0700

    Add one line to this code

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,11 +1,16 @@
 #include "main.h"
 
+/**
+ * main - function that holds integers
+ * Return: nothing
+ */
 int main(void)
 {
 	int a[] = {98, 1024, 402, -1024, 0};
 	int *p;
 
 	p = a;
+	*(a + 2) = 98;
 
 	/* Add one line of code here */
 
