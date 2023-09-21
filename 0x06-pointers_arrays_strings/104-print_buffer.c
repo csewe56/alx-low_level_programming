dbf8f9cb759e5a1e0314281ca223d39b78eafb95
csewe56 <csewe56@gmail.com>
2023-09-21 03:26:48 -0700

    a function that prints an integer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -6,44 +6,42 @@
  * @b: The buffer to print.
  * @size: The size of the buffer.
  */
+
 void print_buffer(char *b, int size)
 {
-    int i, j;
+	int i, j;
 
-    if (size <= 0)
-    {
-        putchar('\n');
-        return;
-    }
+	if (size <= 0)
+	{
+		putchar('\n');
+	}
 
-    for (i = 0; i < size; i += 10)
-    {
-        printf("%08x: ", i);
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
 
-        for (j = 0; j < 10; j++)
-        {
-            if (i + j < size)
-                printf("%02x", (unsigned char)b[i + j]);
-            else
-                printf("  ");
+	for (j = 0; j < 10; j++)
+	{
+		if (i + j < size)
+			printf("%02x", (unsigned char)b[i + j]);
+		{
+		printf("  ");
 
-            if (j % 2 == 1)
-                printf(" ");
-        }
+		if (j % 2 == 1)
+			printf(" ");
+		{
 
-        for (j = 0; j < 10; j++)
-        {
-            if (i + j < size)
-            {
-                if (b[i + j] >= 32 && b[i + j] <= 126)
-                    putchar(b[i + j]);
-                else
-                    putchar('.');
-            }
-        }
-
-        putchar('\n');
-    }
-}
-
-
+	for (j = 0; j < 10; j++)
+	{
+		if (i + j < size)
+		{
+			if (b[i + j] >= 32 && b[i + j] <= 126)
+				putchar(b[i + j]);
+			else
+				putchar('.');
+		}
+	}
+	putchar('\n');
+		}
+		return
+		}
