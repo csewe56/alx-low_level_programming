7d51cc452c6b30b65f50fa7d5fccc8bdf3f2775c
csewe56 <csewe56@gmail.com>
2023-09-21 05:01:09 -0700

    a function that prints an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,11 +10,14 @@ void print_number(int n)
 	{
 		_putchar('-');
 		n = -n;
-		_putchar(n + '0');
 	}
-	else
+
+	if (n / 10 != 0)
 	{
-		_putchar(n + '0');
+		print_number(n / 10);
 	}
+
+	_putchar((n % 10) + '0');
 }
+
 
