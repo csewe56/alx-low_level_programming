ede9548a805ef3ff1551fb8a56e1c8cda89acea3
csewe56 <csewe56@gmail.com>
2023-09-22 05:12:03 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -3,8 +3,8 @@
 /**
  * main - Entry point
  *
- * Program sets a[2] to 98 without using 'a' or modifying 'p'.
- * It accomplishes this with a single statement.
+ * Description: This is the main function that initializes
+ * variables, manipulates them, and prints the result.
  *
  * Return: Always 0 (Success)
  */
@@ -17,11 +17,21 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
 
-	p[5] = 98;
+	/*
+	 * write your line of code here...
+	 * Remember:
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
+	 */
+	*(p + 5) = 98;
 
+	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
+
 
 
 
