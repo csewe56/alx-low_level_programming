9c074b9af39b46bed0d95f5a0aefb935a23366ea
csewe56 <csewe56@gmail.com>
2023-10-10 03:55:17 -0700

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -24,6 +24,9 @@ typedef struct dog dog_t;
  * @d: Pointer to a struct dog
  */
 void print_dog(dog_t *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */
 
