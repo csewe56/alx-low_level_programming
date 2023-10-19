75e489cc343cc2d5b71e9f7a7a651a411173bb84
csewe56 <csewe56@gmail.com>
2023-10-19 04:54:35 -0700

    0x12. C - Singly linked lists

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,29 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * struct list_s - Singly linked list
+ * @str: String (char *)
+ * @len: Length of the string
+ * @next: Points to the next node
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+/* Function prototypes */
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+int _putchar(char c);
+
+#endif /* LISTS_H */
+
