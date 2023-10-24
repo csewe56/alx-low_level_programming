6e1f46673dafadf42cae4568982bddd2f0702039
csewe56 <csewe56@gmail.com>
2023-10-24 12:17:17 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -6,27 +6,32 @@
  * print_listint_safe - Prints a listint_t linked list.
  * @head: A pointer to the head of the list.
  *
- * Return: The number of nodes in the list.
- * If it fails, exit with status 98.
+ * Return: The number of nodes in the list. If it fails, exit with status 98.
  */
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t node_count = 0;
+	const listint_t *slow_ptr, *fast_ptr;
 
-	while (head != NULL)
+	slow_ptr = head;
+	fast_ptr = head;
+
+	while (slow_ptr && fast_ptr && fast_ptr->next)
 	{
-		printf("%d\n", head->n);
+		slow_ptr = slow_ptr->next;
+		fast_ptr = fast_ptr->next->next;
+
+		printf("%d\n", slow_ptr->n);
 		node_count++;
 
-		if (head < head->next)
+		if (slow_ptr == fast_ptr)
 		{
-			fprintf(stderr, "Linked list loop detected\n");
+			fprintf(stderr, "-> [%p] %d\n", (void *)slow_ptr, slow_ptr->n);
 			exit(98);
 		}
-
-		head = head->next;
 	}
 
 	return (node_count);
 }
+
 
